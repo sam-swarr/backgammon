695713dba29d68bd35f3959a6a0ae91fe84f3f94
Sam Swarr <swarr687@gmail.com>
2021-09-30 16:28:34 -0400

    initial commit

---
+++ src/GameBoard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class GameBoard extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Game-board-wrapper">
+        Backgammon
+      </div>
+    );
+  }
+}
